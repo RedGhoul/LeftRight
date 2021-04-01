a8fff28a63ed12f0e1159b9003e858bf0a491c24
RedGhoul <avaneesab5@gmail.com>
2021-03-31 20:03:39 -0400

    Added Time Stamp

--- database/SQL/base.sql
+++ database/SQL/base.sql
@@ -2,12 +2,12 @@ CREATE TABLE newssite (
 	id serial PRIMARY KEY,
 	name VARCHAR ( 600 ) NOT NULL,
 	url VARCHAR ( 600 ) NOT NULL,
-	created_on Date not null default CURRENT_DATE
+	created_on timestamp not null default NOW()::timestamp
 );
 CREATE TABLE snapshot (
 	id serial PRIMARY KEY,
 	imageurl VARCHAR ( 600 ) NOT NULL,
-	created_on Date not null default CURRENT_DATE,
+	created_on timestamp not null default NOW()::timestamp,
     newssite_id INT NOT NULL,
     CONSTRAINT snapshot_newssite_id_fkey FOREIGN KEY (newssite_id)
         REFERENCES newssite (id) MATCH SIMPLE
@@ -18,7 +18,7 @@ CREATE TABLE snapshot (
 CREATE TABLE headline (
     id serial PRIMARY KEY,
     value_text text NOT NULL,
-    created_on Date not null default CURRENT_DATE,
+    created_on timestamp not null default NOW()::timestamp,
     snapshot_id INT NOT NULL,
     value_text_sentiment text NOT NULL,
     CONSTRAINT headline_snapshot_id_fkey FOREIGN KEY (snapshot_id)
