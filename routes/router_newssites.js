d5405726b824dd67fd3ac2df05f1e0b43b58ebed
RedGhoul <avaneesab5@gmail.com>
2021-04-09 21:43:13 -0400

    gutted the whole thing

--- routes/router_newssites.js
+++ routes/router_newssites.js
@@ -1,4 +1,152 @@
 const client = require('../database/database');
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+
+const GetHeadLine = async (req, res) => {
+    let Name = req.body.Name;
+    let URL = req.body.Url;
+    if (!Name || !URL) {
+        return res.json({ error: "error" });
+    }
+
+    const browser = await puppeteer.launch({
+        args: ['--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--disable-setuid-sandbox',
+            '--no-first-run',
+            '--no-sandbox',
+            '--no-zygote',
+            '--single-process'],
+        headless: true
+    });
+    const page = await browser.newPage();
+
+    await page.setDefaultNavigationTimeout(90000);
+
+    try {
+        await page.goto(URL, {
+            waitUntil: 'networkidle2'
+        });
+    } catch (error) {
+        await browser.close();
+        return res.json({ error: error });
+    }
+
+    const data = await page.evaluate(() => document.querySelector('*').outerHTML);
+    if (!data) {
+        console.log("Data was none");
+        console.log(data);
+        await browser.close();
+        return res.json({ error: "Data was none" });
+    }
+
+    const $ = cheerio.load(data);
+
+    let listOfHeadLines = []
+    if (Name === 'CNN') {
+        const stuff = $(".cd__headline-text");
+        for (i = 0; i < stuff.length; i++) {
+            try {
+                if (stuff[i].children[0].data) {
+                    let headline = stuff[i].children[0].data.trim();
+                    listOfHeadLines.push(headline);
+
+                }
+            } catch (error) {
+                console.log("Happend in For Loop");
+                console.log("Error found in CNN");
+                console.log(error);
+            }
+
+        }
+    } else if (Name === 'HuffPost') {
+        const stuff = $(".card__headline__text");
+        for (i = 0; i < stuff.length; i++) {
+            try {
+                if (stuff[i].children[0].data) {
+                    let headline = stuff[i].children[0].data.trim();
+
+                }
+            } catch (error) {
+                console.log("Happend in For Loop");
+                console.log("Error found in HuffPost");
+                console.log(error);
+            }
+
+        }
+    } else if (Name === 'Fox') {
+        const stuff = $(".title.title-color-default");
+        for (i = 0; i < stuff.length; i++) {
+            try {
+                if (stuff[i].children[0].children[0].data) {
+                    let headline = stuff[i].children[0].children[0].data.trim();
+
+                }
+            } catch (error) {
+                console.log("Happend in For Loop");
+                console.log("Error found in Fox");
+                console.log(error);
+            }
+
+        }
+    } else if (Name === 'FT') {
+        const stuff = $(".js-teaser-heading-link");
+        for (i = 0; i < stuff.length; i++) {
+            try {
+                if (stuff[i].children[0].data) {
+                    let headline = stuff[i].children[0].data.trim();
+
+                }
+            } catch (error) {
+                console.log("Happend in For Loop");
+                console.log("Error found in FT");
+                console.log(error);
+            }
+
+        }
+    } else if (Name === 'BBC') {
+        const stuff = $(".media__link");
+        for (i = 0; i < stuff.length; i++) {
+            try {
+                if (stuff[i].children[0].data) {
+                    let headline = stuff[i].children[0].data.trim();
+
+                }
+            } catch (error) {
+                console.log("Happend in For Loop");
+                console.log("Error found in BBC");
+                console.log(error);
+            }
+
+        }
+    } else if (Name === 'washingtonpost') {
+        const stuff = $(".font--headline");
+        console.log(stuff);
+        if (!stuff) {
+            console.log("Error occured in washingtonpost for loop");
+            console.log(stuff);
+            return res.json({ error: "error" });
+        }
+        for (i = 0; i < stuff.length; i++) {
+            try {
+                if (stuff[i] || stuff[i].children || stuff[i].children[0].children[0].children[0].data) {
+                    let headline = stuff[i].children[0].children[0].children[0].data.trim();
+                }
+
+            } catch (error) {
+                console.log("Happend in For Loop - stuff[i].children[0].children[0].children[0].data");
+                console.log("Error found in washingtonpost");
+                console.log(error);
+            }
+
+
+        }
+    }
+
+    await browser.close();
+    return res.json({ HeadLines: listOfHeadLines });
+}
+
 
 const GetCreateNewsSite = (req, res) => {
     return res.render('news_sites/create_news_site.ejs');
@@ -53,5 +201,6 @@ module.exports = {
     GetNewsSites: GetNewsSites,
     UpdateNewsSites: UpdateNewsSites,
     DeleteNewsSites: DeleteNewsSites,
-    GetUpdateNewsSitesForm: GetUpdateNewsSitesForm
+    GetUpdateNewsSitesForm: GetUpdateNewsSitesForm,
+    GetHeadLine: GetHeadLine
 }
--- server.js
+++ server.js
@@ -4,55 +4,21 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express');
 const app = express();
-
-const passport = require('passport');
-const flash = require('express-flash');
-const session = require('express-session');
 const methodOverride = require('method-override');
-const { Router, StartProcesses } = require('./tasks/tasks');
-const { GetCreateNewsSite, PostCreateNewsSite,
-  GetNewsSites, DeleteNewsSites, UpdateNewsSites, GetUpdateNewsSitesForm } = require('./routes/router_newssites');
 
-const { GetLoginPage, GetRegister,
-  PostRegister, Logout, GetHomePage } = require('./routes/router_auth');
+const { GetHeadLine } = require('./routes/router_newssites');
 
-const { checkAuthenticated,
-  checkNotAuthenticated, addLoginFlag } = require('./middleware/middleware');
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
-const initializePassport = require('./auth/passport-config');
-
-initializePassport(passport);
 app.set('view-engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
-app.use(flash());
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(methodOverride('_method'));
-app.use(addLoginFlag);
-app.get('/', GetHomePage);
-app.get('/Login', checkNotAuthenticated, GetLoginPage);
-app.post('/Login', checkNotAuthenticated, passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureFlash: true
-}));
-app.get('/register', checkNotAuthenticated, GetRegister);
-app.post('/register', checkNotAuthenticated, PostRegister);
-app.delete('/logout', Logout);
-app.get("/NewsSite/All", checkAuthenticated, GetNewsSites);
-app.get("/NewsSite/Create", checkAuthenticated, GetCreateNewsSite);
-app.post("/NewsSite/Create", checkAuthenticated, PostCreateNewsSite);
-app.delete('/NewsSite/Delete/:id', checkAuthenticated, DeleteNewsSites);
-app.get('/NewsSite/Update/:id', checkAuthenticated, GetUpdateNewsSitesForm);
-app.post('/NewsSite/Update/:id', checkAuthenticated, UpdateNewsSites);
-app.use('/admin/queues', checkAuthenticated, Router);
 
-StartProcesses();
+app.use(methodOverride('_method'));
+
+app.post('/headline', checkNotAuthenticated, GetHeadLine);
 
 app.listen(process.env.PORT, '0.0.0.0', () => {
   console.log("Connected ! " + process.env.PORT + ' 0.0.0.0');
