13eb55044d24a7dd8d126a7ab2043267fe2c3e73
RedGhoul <avaneesab5@gmail.com>
2021-07-31 08:23:14 -0400

    added port number for mysql

--- database/database.js
+++ database/database.js
@@ -3,6 +3,7 @@ var pool = mysql.createPool({
     connectionLimit: process.env.MYSQL_CONNECTIONS,
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
+    port: process.env.MYSQL_PORT,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DBNAME
 });
